05024428c1a7dfe614d8eae25240a090d0e0eb54
xsxinghen <84395857+xsxinghen@users.noreply.github.com>
2026-08-22 23:37:10 +0800

    Update footer links in _worker.js

--- _worker.js
+++ _worker.js
@@ -525,8 +525,8 @@ async function githubInterface() {
              		   	<h4>© 2026 星痕. All Rights Reserved. 
                 		<br>
                 		由
-                		<a target="_blank" href="https://github.com/Geekertao/CF-Workers-GitHub-Proxy/">CF-Workers-GitHub</a> 提供技术支持 & 部署于
-                		<a target="_blank" href="https://www.cloudflare.com/">Cloudflare pages</a>
+                		<a target="_blank" href="https://github.com/cmliu/CF-Workers-GitHub/">CF-Workers-GitHub</a> 提供技术支持 & 部署于
+                		<a target="_blank" href="https://www.cloudflare.com/">Cloudflare pages</a> 上.
             		</div>
         		</div>
 			</div>
